a5c39ef5320d413a2fddfdf64b95ab361a9791da
virtual photon <zphoton0@gmail.com>
2020-09-19 13:25:35 +0300

    added more efficient version of 03 (nth bit) and 07 (power of 2)

--- lesson_01/03_find_nth_bit_in_number.cpp
+++ lesson_01/03_find_nth_bit_in_number.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int main(){
-	int num, pos;
-	std::cin >> num >> pos;
-	std::cout << ((num & (1 << pos)) >> pos) << '\n';
-	return 0;
+int main() {
+    int num, pos;
+    std::cin >> num >> pos;
+    std::cout << (num >> pos & 1) << '\n';
+    return 0;
 }
--- lesson_01/07_test_if_power_of_2.cpp
+++ lesson_01/07_test_if_power_of_2.cpp
@@ -2,16 +2,14 @@
 
 using std::cout;
 
-int main(){
-	int num;
-	std::cin >> num;
-	do {
-		if (!num or num & 1 and num >> 1){
-			cout << "no" << '\n';
-			return 0;
-		}
-		num >>= 1;
-	} while(num);
-	cout << "yes" << '\n';
-	return 0;
+int main() {
+    int num;
+    std::cin >> num;
+    int is_power = 0;
+    for (int _ = 0; _ < 32; _ += 1) {
+        is_power ^= num;
+        num >>= 1;
+    }
+    cout << ((is_power & 1 and !(num>>31 & 1)) ? "yes":"no");
+    return 0;
 }
