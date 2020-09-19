81d5b11de4cb84d41e3cdfd1f92f29969ae4d77d
virtual photon <zphoton0@gmail.com>
2020-09-19 10:33:14 +0300

    added 07 that checks if number is power of 2

---
+++ 07_test_if_power_of_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+
+int main(){
+	int num;
+	std::cin >> num;
+	do {
+		if (!num or num & 1 and num >> 1){
+			cout << "no" << '\n';
+			return 0;
+		}
+		num >>= 1;
+	} while(num);
+	cout << "yes" << '\n';
+}
