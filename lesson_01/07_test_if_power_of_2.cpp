7f466eab27ce3261ccebb365223329f3c982b725
virtual photon <zphoton0@gmail.com>
2020-09-19 14:03:49 +0300

    added even better solution for 07(if num is power of 2)

--- lesson_01/07_test_if_power_of_2.cpp
+++ lesson_01/07_test_if_power_of_2.cpp
@@ -5,11 +5,7 @@ using std::cout;
 int main() {
     int num;
     std::cin >> num;
-    int is_power = 0;
-    for (int _ = 0; _ < 32; _ += 1) {
-        is_power ^= num;
-        num >>= 1;
-    }
-    cout << ((is_power & 1 and !(num>>31 & 1)) ? "yes":"no");
+    num = num & (num-1);
+    std::cout << (num? "no":"yes");
     return 0;
 }
