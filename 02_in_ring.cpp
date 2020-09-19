6f79f81266552e5e5686b264c7c561cfb2c2ae19
photon <zphoton0@gmail.com>
2020-09-19 09:15:58 +0300

    added working 02
    it finds if dot is in ring

---
+++ 02_in_ring.cpp
@@ -0,0 +1,14 @@
+/*
+input:
+	x, y, r, R
+*/
+#include <iostream>
+#include <cmath>
+
+
+int main(int argc, char *argv[]){
+	double x, y, r, R;
+	std::cin >> x >> y >> r >> R;
+	double ro = std::sqrt(x*x + y*y);
+	std::cout << ((ro >= r && ro <= R)? "yes":"no") << '\n'; 
+}
