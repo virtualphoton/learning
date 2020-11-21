279ef7e3f3c5d34a10e29ffa9d4b17282ace7870
virtual photon <zphoton0@gmail.com>
2020-11-21 14:18:12 +0300

    bogo sort like update

--- lesson_12_sorts/best_sort_in_de_world.cpp
+++ lesson_12_sorts/best_sort_in_de_world.cpp
@@ -1,17 +1,14 @@
 #include <time.h>
 
 template <class T>
-void bogo_sort_like(T * arr, int len, T * comp) {
-    srand(time(NULL));
-    for (int i = 0; i < len; i += 1)
-        comp[i] = arr[i];
+void bogo_sort_like(T * arr, int len) {
     int i;
     while (true) {
         for (i = 1; i < len; i += 1)
-            if (comp[i] < comp[i - 1])
+            if (arr[i] < arr[i - 1])
                 break;
         if (i == len)
             break;
-        std::swap(comp[rand() % len], comp[rand() % len]);
+        std::swap(arr[rand() % len], arr[rand() % len]);
     }
 }
