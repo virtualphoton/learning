4b2b40655cb36692a9c97ece51c310bb52b322d5
photon <zphoton0@gmail.com>
2021-06-19 21:15:23 +0300

    brackets no recursion

--- brac/ne_slabO.cpp
+++ brac/ne_slabO.cpp
@@ -6,24 +6,23 @@
 using std::string;
 bool is_valid(string s) {
 	std::map<char, char> pairs{ {'(', ')'}, {'{', '}'}, {'[', ']'} };
-	for (int start = 0; start < s.size(); start += 1)
-		for (int i = start; i < s.size(); i += 1)
-			if (s[i] != 'ø') {
-				if (pairs.find(s[i]) == pairs.end())
-					return false;
-				int counter = 0, j;
-				for (j = i; j < s.size(); j += 1) {
-					if (s[j] == pairs[s[i]])
-						counter -= 1;
-					if (s[j] == s[i])
-						counter += 1;
-					if (not counter or s[j] == 'ø')
-						break;
-				}
-				if (counter)
-					return false;
-				s[i] = s[j] = 'ø';
+	for (int i = 0; i < s.size(); i += 1)
+		if (s[i] != 'ø') {
+			if (pairs.find(s[i]) == pairs.end())
+				return false;
+			int counter = 0, j;
+			for (j = i; j < s.size(); j += 1) {
+				if (s[j] == pairs[s[i]])
+					counter -= 1;
+				if (s[j] == s[i])
+					counter += 1;
+				if (not counter or s[j] == 'ø')
+					break;
 			}
+			if (counter)
+				return false;
+			s[i] = s[j] = 'ø';
+		}
 	return true;
 }
 
